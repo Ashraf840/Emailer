dac1a1b5050218f131830b1496ee9b3b63eb7f3a
Tanjim <shift2unleashed2011@gmail.com>
2021-01-15 17:26:05 +0600

    Custom File-Upload Button

--- SendEmailApp/forms.py
+++ SendEmailApp/forms.py
@@ -4,16 +4,26 @@ from django import forms
 # Custom Email Sending Form
 class EmailForm(forms.Form):
     DESIGNATION_CHOICES = [
-    ('Student', 'Student'),
-    ('Teacher', 'Teacher'),
-    ('Lawyer', 'Lawyer'),
-    ('Business', 'Business'),
-]
+        ('Student', 'Student'),
+        ('Teacher', 'Teacher'),
+        ('Lawyer', 'Lawyer'),
+        ('Business', 'Business'),
+    ]
 
     fullname = forms.CharField(label='Your Name', max_length=50, widget=forms.TextInput())
-    company = forms.CharField(label='Company Name', max_length=50, widget=forms.TextInput())
-    designation = forms.ChoiceField(choices = DESIGNATION_CHOICES) 
+    company = forms.CharField(label='Company Name', max_length=50, required=False, widget=forms.TextInput())
+    designation = forms.ChoiceField(required=False, choices = DESIGNATION_CHOICES) 
     email = forms.CharField(label='Recipient', max_length=100, widget=forms.TextInput())
     subject = forms.CharField(max_length=255, widget=forms.TextInput())
     message = forms.CharField(max_length=500, widget=forms.Textarea())
     attachment = forms.FileField(label='', required=False, widget=forms.ClearableFileInput(attrs={ 'multiple':True }))
+
+    def __init__(self, *args, **kwargs):
+        super(EmailForm, self).__init__(*args, **kwargs)
+        self.fields['attachment'].widget.attrs['style'] = 'background-color: red;'      # not displaying the main file-upload btn, hide it on-page
+        self.fields['attachment'].widget.attrs['id'] = 'upfile'
+        self.fields['attachment'].widget.attrs['onchange'] = 'sub(this)'
+
+        # NB: With this method, can add PLACEHOLDER to the form fields.
+
+    
